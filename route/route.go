8ab8f78645787a1937a0e8b7268341775e74dccc
atb <atb@odds.team>
2019-12-27 09:55:38 +0700

    create path to get seller users

---
+++ route/route.go
@@ -0,0 +1,20 @@
+package route
+
+import (
+	"github.com/labstack/echo"
+	"gitlab.odds.team/internship/sec-kinkao/goback/api"
+)
+
+func Init(e *echo.Echo) {
+	db, err := api.NewMongoDB()
+	if err != nil {
+		e.Logger.Fatal(err)
+	}
+
+	a := &api.MongoDB{
+		Conn:       db.Conn,
+		SellerUCol: db.SellerUCol,
+	}
+
+	e.GET("/sellerusers", a.GetSellerUsers)
+}
