787f6b82cd3f016faf07aaf803105829bdd052c8
atb <atb@odds.team>
2019-12-27 09:54:20 +0700

    use middleware CORS, Recover and Logger

--- server.go
+++ server.go
@@ -2,14 +2,30 @@ package main
 
 import (
 	"net/http"
-	
+
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"github.com/labstack/gommon/log"
+	"gitlab.odds.team/internship/sec-kinkao/goback/config"
+	"gitlab.odds.team/internship/sec-kinkao/goback/route"
 )
 
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+	s := config.Spec()
+
+	e.Logger.SetLevel(log.ERROR)
+	e.Use(
+		middleware.CORS(),
+		middleware.Recover(),
+		middleware.Logger(),
+	)
+
+	e.GET("/_ah/health", func(c echo.Context) error {
+		return c.String(http.StatusOK, "sec kinkao goback : ok!")
 	})
-	e.Logger.Fatal(e.Start(":1323"))
+
+	route.Init(e)
+
+	e.Logger.Fatal(e.Start(s.APIPort))
 }
