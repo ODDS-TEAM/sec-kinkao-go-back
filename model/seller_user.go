c9e31ef178d0947de3103afdb64f2a710878bf3d
atb <atb@odds.team>
2019-12-27 16:12:18 +0700

    define json and bson

--- model/seller_user.go
+++ model/seller_user.go
@@ -2,19 +2,22 @@ package model
 
 import "gopkg.in/mgo.v2/bson"
 
+import "time"
+
 type (
 	SellerUser struct {
-		ID        bson.ObjectId
-		Username  string
-		Password  string
-		Email     string
-		StoreInfo StoreInfo
+		ID        bson.ObjectId `bson:"_id" json:"_id"`
+		Username  string        `bson:"username" json:"username"`
+		Password  string        `bson:"password" json:"password"`
+		Email     string        `bson:"email" json:"email"`
+		CreatedAt time.Time     `bson:"createAt" json:"createAt"`
+		StoreInfo StoreInfo     `bson:"storeInfo" json:"storeInfo"`
 	}
 	StoreInfo struct {
-		Name        string
-		Owner       string
-		Describe    string
-		PhoneNumber string
-		ImgUrl      string
+		Name        string `bson:"name" json:"name"`
+		Owner       string `bson:"owner" json:"owner"`
+		Describe    string `bson:"describe" json:"describe"`
+		PhoneNumber string `bson:"phoneNumber" json:"phoneNumber"`
+		ImgUrl      string `bson:"imgUrl" json:"imgUrl"`
 	}
 )
