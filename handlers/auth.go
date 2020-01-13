f41b835d54066c0134a48d8d670628459ed057b5
atb <p_athibet@odds.team>
2020-01-13 10:59:49 +0700

    create login fuction

--- handlers/auth.go
+++ handlers/auth.go
@@ -1,9 +1,29 @@
 package handlers
 
-import "github.com/labstack/echo"
+import (
+	"net/http"
 
-import "net/http"
+	"github.com/labstack/echo"
+	"gitlab.odds.team/internship/sec-kinkao/goback/models"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
 
 func (h *Handler) Login(c echo.Context) (err error) {
-	return c.JSON(http.StatusOK, {"message": "OK"})
+	u := &models.User{}
+	if err = c.Bind(u); err != nil {
+		return
+	}
+
+	db := h.DB.Clone()
+	defer db.Close()
+	if err = db.DB("twitter").C("users").
+		Find(bson.M{"email": u.Email, "password": u.Password}).One(u); err != nil {
+		if err == mgo.ErrNotFound {
+			return &echo.HTTPError{Code: http.StatusUnauthorized, Message: "invalid email or password"}
+		}
+		return
+	}
+
+	return c.JSON(http.StatusOK, u)
 }
