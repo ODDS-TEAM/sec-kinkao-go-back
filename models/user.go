5eadb182b655a74d3908e0f783e75c8e4d4ecb8d
atb <p_athibet@odds.team>
2020-01-13 11:00:01 +0700

    create login fuction

--- models/user.go
+++ models/user.go
@@ -1,22 +1,21 @@
 package models
 
 import "gopkg.in/mgo.v2/bson"
-import "time"
 
 type (
 	User struct {
-		ID        bson.ObjectId `bson:"_id" json:"_id"`
-		Username  string        `bson:"username" json:"username"`
-		Password  string        `bson:"password" json:"password"`
-		Email     string        `bson:"email" json:"email"`
-		CreatedAt time.Time     `bson:"createAt" json:"createAt"`
-		StoreInfo Info          `bson:"storeInfo" json:"storeInfo"`
+		ID       bson.ObjectId `bson:"_id" json:"_id"`
+		Username string        `bson:"username" json:"username"`
+		Password string        `bson:"password" json:"password"`
+		Email    string        `bson:"email" json:"email"`
 	}
-	Info struct {
-		Name        string `bson:"name" json:"name"`
-		Owner       string `bson:"owner" json:"owner"`
-		Describe    string `bson:"describe" json:"describe"`
-		PhoneNumber string `bson:"phoneNumber" json:"phoneNumber"`
-		ImgUrl      string `bson:"imgUrl" json:"imgUrl"`
-	}
+	// CreatedAt time.Time     `bson:"createAt" json:"createAt"`
+	// StoreInfo Info          `bson:"storeInfo" json:"storeInfo"`
+	// Info struct {
+	// 	Name        string `bson:"name" json:"name"`
+	// 	Owner       string `bson:"owner" json:"owner"`
+	// 	Describe    string `bson:"describe" json:"describe"`
+	// 	PhoneNumber string `bson:"phoneNumber" json:"phoneNumber"`
+	// 	ImgUrl      string `bson:"imgUrl" json:"imgUrl"`
+	// }
 )
